ef8a186a5d07f4672da336539a17d3469af521aa
mthariqfaturrahman <79246148+mthariqfaturrahman@users.noreply.github.com>
2024-05-09 09:41:43 +0700

    Update assessment.sh

--- assessment.sh
+++ assessment.sh
@@ -15,3 +15,5 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt install docker-ce -y
+
+echo "done"
